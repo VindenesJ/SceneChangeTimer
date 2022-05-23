01716372cf1849c4fb492cfdce5ffa6d060b517b
Joakim Vindenes <42808141+matriseblog@users.noreply.github.com>
2022-05-23 20:04:37 +0200

    A small project that changes between scenes at a timer set by a coroutine.

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SceneTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator SceneTimer()
+    {
+        float sceneAmount = 5;
+        for(int i = 0; i <= sceneAmount; i++)
+        {
+            yield return new WaitForSeconds(10);
+            SceneManager.LoadScene($"Scene{i}");        }
+    }
+}
